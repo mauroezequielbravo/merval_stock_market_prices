3822ea15e07484d1a77b8d2193b2723e2fd4116e
Mauro Ezequiel Bravo <mauroezequielbravo@hotmail.com>
2024-08-12 00:46:11 -0300

    Se argega la posibilidad de programar la ejecucion de la optencion de los precios.

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
+import schedule
+import time
 from utils.functions.generate_initial_file_tickers import generate_initial_file_tickers
 from utils.functions.tickers import get_ticker_to_consult, get_total_ticker_to_consult, add_price_to_ticker, create_data_file
 from utils.functions.scraper import get_share_price
-import pandas as pd
-import pyarrow as pa
 
 
-generate_initial_file_tickers()
-res = get_ticker_to_consult()
-res_data = get_share_price(res)
-create_data_file()
+def job():
+    generate_initial_file_tickers()
+    res = get_ticker_to_consult()
+    res_data = get_share_price(res)
+    create_data_file()
+    add_price_to_ticker(res_data)
 
-add_price_to_ticker(res_data)
 
+schedule.every(2).minutes.do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
