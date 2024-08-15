e5dc4863ce8c36dbe90f43bbb9491e045702f921
Mauro Ezequiel Bravo <mauroezequielbravo@hotmail.com>
2024-08-14 23:27:06 -0300

    Se agrega log para mostrar que precio se esta capturando.

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 import schedule
 import time
+import logging
 from utils.functions.generate_initial_file_tickers import generate_initial_file_tickers
 from utils.functions.tickers import get_ticker_to_consult, get_total_ticker_to_consult, add_price_to_ticker, create_data_file
 from utils.functions.scraper import get_share_price
 
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 def job():
     generate_initial_file_tickers()
     res = get_ticker_to_consult()
+    logging.info('Obteniendo el precio de: {}'.format(res))
     res_data = get_share_price(res)
     create_data_file()
     add_price_to_ticker(res_data)
